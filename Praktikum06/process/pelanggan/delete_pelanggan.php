8fd13ad5d0ed62ab49d295d723a1830a3eb80f73
PurpleLuvvee <114552163+PurpleLuvvee@users.noreply.github.com>
2023-04-06 22:44:48 +0700

    Delete Praktikum06 directory

--- Praktikum06/process/pelanggan/delete_pelanggan.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once '../../database/dbkoneksi.php';
-?>
-<?php
-    $sql = "DELETE FROM pelanggan WHERE id='$_GET[iddel]'";
-    $rs = $dbh->query($sql);
-
-    header("location:../../pages/pelanggan/list_pelanggan.php");
-?>
--- Praktikum06/process/produk/delete_produk.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../../database/dbkoneksi.php";
-$id = $_GET['iddel'];
-$sql = "DELETE FROM produk WHERE id = ?";
-$statement = $dbh->prepare($sql);
-$statement->execute([$id]);
-
-header("location:../../pages/produk/list_produk.php");
